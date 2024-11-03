4bf1b1ca87787eab913dfb8700e8f941fdd7263f
Austin Booth <austinbooth78@gmail.com>
2024-11-03 12:16:19 +0000

    Add ngx-charts and use

--- app/src/app/components/recent-temperatures/recent-temperatures.component.ts
+++ app/src/app/components/recent-temperatures/recent-temperatures.component.ts
@@ -1,21 +1,37 @@
 import { Component } from '@angular/core';
 import { TemperatureService } from '../../services/temperature/temperature.service';
 import Temperature from '../../types/Temperature';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+
+type ChartData = {
+  name: string;
+  series: {
+    name: string;
+    value: number;
+  }[];
+};
 
 @Component({
   selector: 'app-recent-temperatures',
   standalone: true,
-  imports: [],
+  imports: [NgxChartsModule],
   templateUrl: './recent-temperatures.component.html',
   styleUrl: './recent-temperatures.component.scss'
 })
 export class RecentTemperaturesComponent {
-  temperatures: Temperature[] = [];
+  chartData: ChartData[] = [];
 
   constructor(private temperatureService: TemperatureService) {
     this.temperatureService.getRecentData('1234').subscribe((data: Temperature[]) => {
-      this.temperatures = data;
-      console.log(this.temperatures);
+      this.chartData = [
+        {
+          name: 'Temperature',
+          series: data.map((temperature: Temperature) => ({
+            name: temperature.timestamp,
+            value: temperature.temperature
+          }))
+        }
+      ];
     });
   }
 }
