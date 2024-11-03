03be43351e238f1986ba9dc13f5a67b97fd61f7b
Austin Booth <austinbooth78@gmail.com>
2024-11-03 13:21:17 +0000

    Add query params to getAverageData

--- app/src/app/services/temperature/temperature.service.ts
+++ app/src/app/services/temperature/temperature.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import Temperature from '../../types/Temperature';
 import AverageTemperature from '../../types/AverageTemperature';
+
+type GetAverageDataParams = {
+  deviceId: string;
+  startDate: string;
+  endDate: string;
+};
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +20,10 @@ export class TemperatureService {
     return this.http.get<Temperature[]>(`http://localhost:5555/api/temperature/recent/${deviceId}`);
   }
 
-  getAverageData(deviceId: string): Observable<AverageTemperature[]> {
-    return this.http.get<AverageTemperature[]>(`http://localhost:5555/api/temperature/average/${deviceId}`);
+  getAverageData({deviceId, startDate, endDate}: GetAverageDataParams): Observable<AverageTemperature[]> {
+    const params = new HttpParams()
+      .set('startDate', startDate)
+      .set('endDate', endDate);
+    return this.http.get<AverageTemperature[]>(`http://localhost:5555/api/temperature/average/${deviceId}`, { params });
   }
 }
